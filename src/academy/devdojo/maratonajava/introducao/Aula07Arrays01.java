8806d2f4e8e35c1739aa85bb3e5e76441bd118e7
josefernandosilva2007 <josefernandosilva0501@gmail.com>
2025-02-01 14:02:26 -0300

    update

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int[] idade = new int[3];
+        idade[0] = 18;
+        idade[1] = 17;
+        idade[2] = 19;
+        System.out.println(idade[2]);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        //byte, short, long, int, float e double = 0
+        //char ' '
+        //boolean false
+        //String null
+        int numeroArray = 4;
+        String[] nomes = new String[numeroArray];
+        nomes[0] = "Subaru";
+        nomes[1] = "Mitsubish";
+        nomes[2] = "Corvette";
+        nomes[3] = "Fiat Uno";
+
+        for (int i = 0; i < numeroArray ; i++) {
+            System.out.println(nomes[i]);
+
+        }
+
+    }
+}
